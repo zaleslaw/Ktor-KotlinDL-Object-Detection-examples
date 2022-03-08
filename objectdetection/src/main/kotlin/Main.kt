1b47c049b7517caa37f06b5f43bf7ab143460082
Michal Harakal <michal.harakal@googlemail.com>
2022-03-08 23:33:03 +0100

    Paint rectangle for detected objects

--- objectdetection/src/main/kotlin/Main.kt
+++ objectdetection/src/main/kotlin/Main.kt
@@ -1,9 +1,20 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.loadImageBitmap
@@ -12,6 +23,7 @@ import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
+import org.jetbrains.kotlinx.dl.api.inference.objectdetection.DetectedObject
 
 @Composable
 @Preview
@@ -30,23 +42,76 @@ fun App(viewModel: ObjectDetectionViewModel = ObjectDetectionViewModel()) {
 
 
     MaterialTheme {
-        Column {
-            Image(
-                painterResource(imageName),
-                contentDescription = null,
-                contentScale = ContentScale.Crop
-            )
-            println("YYYYYYY")
-            val text = when (viewState.detectionState) {
-                DetectionUiStateType.BEGINNING -> "Starting"
-                DetectionUiStateType.LOADING -> "Loading"
-                DetectionUiStateType.DETECTING -> "Detecting"
-                DetectionUiStateType.DONE -> "Done"
+        Row {
+            Column {
+                Box {
+                    Image(
+                        painterResource(imageName),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier.size(1200.dp)
+                    )
+                    if (viewState.detectionState == DetectionUiStateType.DONE) {
+                        SmileyFaceCanvas(viewState.detectedObjects, Size(1200.0f, 400.0f))
+                    }
+                }
+                val text = when (viewState.detectionState) {
+                    DetectionUiStateType.BEGINNING -> "Starting"
+                    DetectionUiStateType.LOADING -> "Loading"
+                    DetectionUiStateType.DETECTING -> "Detecting"
+                    DetectionUiStateType.DONE -> "Done"
+                }
+                Text(text)
+
             }
-            Text(text)
+//                    items(viewState.detectedObjects.size) { detectedObject ->
         }
     }
 }
+
+val green200 = Color(0xFF00B388)
+val green700 = Color(0xFF019370)
+val greenLight200 = Color(0xFFF1FEFB)
+val greenLight700 = Color(0xFFC9EDE4)
+val red700 = Color(0xFFEA3815)
+val blue700 = Color(0xFF043ED6)
+val dark = Color(0xFF101820)
+
+@Composable
+fun SmileyFaceCanvas(
+    objects: List<DetectedObject>,
+    bufferedImage: Size,
+    modifier: Modifier = Modifier
+) {
+    Canvas(
+        modifier = modifier,
+        onDraw = {
+            // Head
+            objects.forEach { obj ->
+                val color = when (obj.classLabel) {
+                    "dog" -> Color.Blue
+                    "car" -> Color.Green
+                    "clock" -> Color.Yellow
+                    "bicycle" -> Color.Magenta
+                    "person" -> Color.Red
+                    else -> Color.White
+                }
+                val top = obj.yMin * bufferedImage.height
+                val left = obj.xMin * bufferedImage.width
+                val bottom = obj.yMax * bufferedImage.height
+                val right = obj.xMax * bufferedImage.width
+
+                if ((color != Color.White) && (obj.probability > 0.3)) {
+                    drawLine(color, Offset(left, top), Offset(right, top))
+                    drawLine(color, Offset(right, top), Offset(right, bottom))
+                    drawLine(color, Offset(right, bottom), Offset(left, bottom))
+                    drawLine(color, Offset(left, bottom), Offset(left, top))
+                }
+            }
+        }
+    )
+}
+
 
 fun main() = singleWindowApplication(
     title = "Object detection",
