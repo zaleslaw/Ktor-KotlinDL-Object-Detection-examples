93f79b861589507de56df3073e0f433ef027b736
Michal Harakal <michal.harakal@googlemail.com>
2022-03-08 23:33:28 +0100

    Remove unused color definitions

--- objectdetection/src/main/kotlin/Main.kt
+++ objectdetection/src/main/kotlin/Main.kt
@@ -69,14 +69,6 @@ fun App(viewModel: ObjectDetectionViewModel = ObjectDetectionViewModel()) {
     }
 }
 
-val green200 = Color(0xFF00B388)
-val green700 = Color(0xFF019370)
-val greenLight200 = Color(0xFFF1FEFB)
-val greenLight700 = Color(0xFFC9EDE4)
-val red700 = Color(0xFFEA3815)
-val blue700 = Color(0xFF043ED6)
-val dark = Color(0xFF101820)
-
 @Composable
 fun SmileyFaceCanvas(
     objects: List<DetectedObject>,
